97c4bd914335a8c3221fd3efc6f209154ae930b1
Teng Qiu <teng.qiu@gmail.com>
2016-02-26 16:27:47 +0000

    Upgrade to 1.6.1-SNAPSHOT and fix toree kernel settings

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-sed -i "s%SPARK_MASTER%$1%g" /tmp/kernel.json
-mkdir -p /root/.local/share/jupyter/kernels/spark
-mv /tmp/kernel.json /root/.local/share/jupyter/kernels/spark/kernel.json
-
 export SPARK_HOME=$SPARK_DIR
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.9-src.zip:$PYTHONPATH
+
+sed -i "s%\${SPARK_MASTER}%$1%g" /tmp/kernel.json
+sed -i "s%\${SPARK_HOME}%$SPARK_HOME%g" /tmp/kernel.json
+sed -i "s%\${PYTHONPATH}%$PYTHONPATH%g" /tmp/kernel.json
+mkdir -p /root/.local/share/jupyter/kernels/spark
+mv /tmp/kernel.json /root/.local/share/jupyter/kernels/spark/kernel.json
 
 mkdir /opt/jupyter
 cd /opt/jupyter
